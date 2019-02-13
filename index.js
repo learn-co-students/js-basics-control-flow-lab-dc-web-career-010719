e9156315ca9b3cc1033895b59feb6247c19d2d56
mkay0961 <mkay0961@gmail.com>
2019-02-12 21:52:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your code in this file!
+console.log("askjdh");
+function scuberGreetingForFeet(x){
+  console.log(x);
+  if (x<= 400){
+    result = "This one is on me!";
+
+  }else if(x>2000 && x < 2500){
+    result = "I will gladly take your thirty bucks.";
+
+  }else if(x>2500){
+    result = "No can do.";
+
+  }
+
+  return result;
+
+}
+function ternaryCheckCity(x ) {
+y = (x == 'NYC') ? "Ok, sounds good.":"No go.";
+return y
+
+
+
+
+
+}
+
+function switchOnCharmFromTip(x) {
+
+    switch(x) {
+    case 'generous':
+      result = "Thank you so much.";
+      break;
+    case 'not as generous':
+      result = "Thank you.";
+      break;
+    default:
+      result = "Bye.";
+  }
+    return result;
+}
