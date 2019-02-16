0e9b17b9b164d7d0db7b7b14dd0fda4699f083c7
canikwe <c.anikwe@gmail.com>
2019-02-16 06:53:08 -0500

    Use if..else if..else and ternery conditional expressions

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet) {
+    
+    
+    if (feet < 400) {
+        return 'This one is on me!'
+    }
+    
+    if (feet > 2000 && feet < 2500) {
+        return 'I will gladly take your thirty bucks.'
+        
+    }
+    
+    if (feet > 2500) {
+        return 'No can do.'
+    }
+    
+}
+
+function ternaryCheckCity(city) {
+    let result
+    city ===  'NYC' ? (result = "Ok, sounds good.") :  (result = "No go.")
+    return result
+}
+
+function switchOnCharmFromTip(tip) {
+
+    if (tip === 'generous') {
+        return 'Thank you so much.'
+    }
+    
+    else if (tip === 'not as generous') {
+        return 'Thank you.'
+    }
+    
+    else {
+        return 'Bye.'
+    }
+
+    
+}
