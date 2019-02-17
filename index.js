10745b2e13f4160eb1e7b1a4a8ec22b56d6280c9
shannon-nabors <shannon.nabors@gmail.com>
2019-02-17 16:31:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  let message
+
+  if (feet <= 400) {
+    message = 'This one is on me!'
+  } else if (feet > 2500) {
+    message = 'No can do.'
+  } else if (feet > 2000 && feet <= 2500) {
+    message = 'I will gladly take your thirty bucks.'
+  }
+
+  return message
+}
+
+function ternaryCheckCity(city) {
+  let message
+
+  switch (city) {
+    case 'NYC':
+      message = "Ok, sounds good."
+      break
+    default:
+      message = "No go."
+      break
+  }
+
+  return message
+}
+
+function switchOnCharmFromTip(tip) {
+  let message
+
+  switch (tip) {
+    case 'generous':
+      message = "Thank you so much."
+      break
+    case 'not as generous':
+      message = "Thank you."
+      break
+    default:
+      message = "Bye."
+      break
+  }
+
+  return message
+}
