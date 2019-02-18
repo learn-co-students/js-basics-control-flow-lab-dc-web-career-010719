13dd3ed529d0ac9e29d541e171fc1499d0e9a7ec
Dusty211 <kylebenz@hotmail.com>
2019-02-17 20:38:28 -0500

    all tests pass

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your code in this file!
+function scuberGreetingForFeet(input) {
+
+  let output
+
+  if (input <= 400) {
+    output = 'This one is on me!'
+  }
+
+  else if (input >= 2000 && input <= 2500) {
+    output = 'I will gladly take your thirty bucks.'
+  }
+
+  else if (input >= 2500) {
+    output = 'No can do.'
+  }
+
+  return output
+}
+
+function ternaryCheckCity(input) {
+
+  let output
+  input === 'NYC' ? output = 'Ok, sounds good.' : output = 'No go.'
+  return output
+
+}
+
+function switchOnCharmFromTip(input) {
+
+  let output
+
+  if (input === 'generous') {
+    output = 'Thank you so much.'
+  }
+
+  else if (input === 'not as generous') {
+    output = 'Thank you.'
+  }
+
+  else {
+    output = 'Bye.'
+  }
+
+  return output
+}
